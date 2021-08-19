9e237a1e8ceabd246499739184ce8bff579980a6
Apple-Pies <sopheaj04@gmail.com>
2021-08-19 17:39:02 -0400

    fixing imports

---
+++ tabs.py
@@ -0,0 +1,48 @@
+from imports import *
+
+
+class CollapseButton(QtWidgets.QPushButton):
+    def __init__(self, parent=None):
+        super(CollapseButton, self).__init__(parent)
+        self.setIcon(QtGui.QIcon("images/left-arrow.png"))
+
+    def mouseReleaseEvent(self, event):
+        super(CollapseButton, self).mouseReleaseEvent(event)
+        self.setIcon(
+            QtGui.QIcon("images/right-arrow.png" if self.isChecked() else "images/left-arrow.png")
+        )
+
+class TabBar(QtWidgets.QTabBar):
+    def tabSizeHint(self, index):
+        s = QtWidgets.QTabBar.tabSizeHint(self, index)
+        s.transpose()
+        return s
+
+    def paintEvent(self, event):
+        painter = QtWidgets.QStylePainter(self)
+        opt = QtWidgets.QStyleOptionTab()
+
+        for i in range(self.count()):
+            self.initStyleOption(opt, i)
+            painter.drawControl(QtWidgets.QStyle.CE_TabBarTabShape, opt)
+            painter.save()
+
+            s = opt.rect.size()
+            s.transpose()
+            r = QtCore.QRect(QtCore.QPoint(), s)
+            r.moveCenter(opt.rect.center())
+            opt.rect = r
+
+            c = self.tabRect(i).center()
+            painter.translate(c)
+            painter.rotate(90)
+            painter.translate(-c)
+            painter.drawControl(QtWidgets.QStyle.CE_TabBarTabLabel, opt)
+            painter.restore()
+
+
+class TabWidget(QtWidgets.QTabWidget):
+    def __init__(self, *args, **kwargs):
+        QtWidgets.QTabWidget.__init__(self, *args, **kwargs)
+        self.setTabBar(TabBar(self))
+        self.setTabPosition(QtWidgets.QTabWidget.West)
