f2666981d9a08157453b070af453c7ab76ae52eb
Apple-Pies <sopheaj04@gmail.com>
2021-09-30 22:28:32 -0400

    Fixed Todo 'View' option's ui features & made side menu toggle

---
+++ src/todo_side_menu/archive_manager.py
@@ -0,0 +1,5 @@
+from PyQt5 import QtWidgets
+
+class ArchiveManager(QtWidgets.QWidget):
+    """Archived Todos Will be Managed Here."""
+    pass
---
+++ src/todo_side_menu/recycle_bin.py
@@ -0,0 +1,5 @@
+from PyQt5 import QtWidgets
+
+class RecycleBin(QtWidgets.QWidget):
+    """Recycled Todos Will be Managed Here."""
+    pass
